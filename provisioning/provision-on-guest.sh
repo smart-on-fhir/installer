a1ff4a3f1f1e57205002fcf19dde0406abce251f
Travis <travis@Travis-MacBook-Pro.local>
2017-05-20 09:30:32 -0600

    Aligned the provision-on-guest.sh script with the "Native Install on Ubuntu 16.04" instructions

--- provisioning/provision-on-guest.sh
+++ provisioning/provision-on-guest.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-# fetch aptitude updates
+# prepare the host machine
 sudo apt-get update
 
-# install prerequisite packages
-sudo apt-get -y install curl \
-                        git \
-                        python-pycurl \
-                        python-pip \
-                        python-yaml \
-                        python-paramiko \
-                        python-jinja2 \
-                        ssl-cert
+sudo apt-get -y install curl git python-pycurl python-pip python-yaml python-paramiko python-jinja2
 
-# install Ansible
 sudo pip install ansible==2.1.0
 
-# Run Ansible Playbook
-cd /vagrant/provisioning
-ansible-playbook -c local -i 'localhost,' site.yml
+git clone https://github.com/smart-on-fhir/installer
+
+cd installer/provisioning
+
+cp custom_settings_example.yml custom_settings.yml
+
+sudo ansible-playbook -c local -i 'localhost,' -vvvv site.yml
